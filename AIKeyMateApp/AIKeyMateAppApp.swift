fca0448ad798c6f6b88ed1981e27288d0ef8d49e
Yi-Han Chen <yhch3n@gmail.com>
2023-04-09 19:01:23 -0400

    Initial Commit

---
+++ AIKeyMateApp/AIKeyMateAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIKeyMateAppApp.swift
+//  AIKeyMateApp
+//
+//  Created by yihandogs on 2023/4/9.
+//
+
+import SwiftUI
+
+@main
+struct AIKeyMateAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
